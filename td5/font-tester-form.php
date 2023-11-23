63b1db7a25da7fd46a36616c7226bc09217339f1
Corentin <cocobau@hotmail.fr>
2023-11-23 11:59:31 +0100

    Suite exercices td5

--- td5/font-tester-form.php
+++ td5/font-tester-form.php
@@ -3,16 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Tester </title>
+    <title>Form Tester</title>
 </head>
 <body>
-    <form method="POST">
-        <input type="number" name="size">
-        <input type="color" name="color">
-        <textarea name="message" id="message" cols="30" rows="10"></textarea>
-        <button type="submit">Valider</button>
-    </form>
+    <?php 
+        $color = $_POST['color']??'';
+        $size = $_POST['size']??10;
+        $message = $_POST['message']??'';
 
-    <p style="color: <?=$_POST['color']??'black'?>; font-size: <?=$_POST['size']??'15'?>px"><?=$_POST['message']??''?></p>
+        if(isset($_POST['sizevar'])){
+            $sizevar = $_POST['sizevar'];
+            if($sizevar == '+'){
+                $size++;
+            }
+            else{
+                $size--;
+            };
+        };
+        
+    ?>
+    <form method="POST">
+        <input type="number" name="size" value="<?=$size?>">
+        <input type="color" name="color" value="<?=$color?>">
+        <textarea name="message" id="message" cols="30" rows="10"><?=$message?></textarea>
+        <button type="submit">Valider</button>
+        <button type='submit' name="sizevar" value='+'>+</button>
+        <button type='submit' name="sizevar" value='-'>-</button>
+    </form>
+    <p style="color: <?=$color?>;font-size: <?= $size?>px"><?=$message?></p>
 </body>
 </html>
